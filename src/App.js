8e43905a19d344ea40fe7a63bde71b3a2b8936e4
Julian Cuentas <juliancuentas@hotmail.com>
2020-05-14 12:22:21 -0300

    second commit: store created with reducer and App wrapped in the store

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
-import React, {useState} from "react";
+import React from "react";
 
 import "./App.css";
+import Store from './store';
 
 function App() {
 
-  const [tasks] = useState([
-    {id : '1', task : 'Terminar app', complete: false},
-    {id : '2', task : 'Leer 1 hora', complete: false},
-    {id : '3', task : 'Limpiar la pieza', complete: false},
-  ])
   
-
   return (
+    <Store>
     <div className="App">
       <div className='navbar'>
            <h2>Comienza a agregar tus tareas</h2> 
@@ -19,20 +15,20 @@ function App() {
       <form>
         <input 
         type="text" 
-        placeholder="Agrega tu tarea"
+        placeholder="Aun me falta hacer...."
+        value={false}
+        onChange={false}
+        required
         ></input>
-        <input type="submit"></input>
+        <input type="submit" value="Agregar mi tarea"></input>
       </form>
       <div className="tasks">
             <ul>
-              {tasks.map(task => {
-                return (
-                  <li key={task.id}>{task.task}</li>
-                )
-              })}
+              {false}
             </ul>
       </div>
     </div>
+    </Store>
   );
 }
 
