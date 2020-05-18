cee8c4b92f610705c6d174e291412cbdb0a46432
Julian Cuentas <juliancuentas@hotmail.com>
2020-05-18 08:12:17 -0300

    Updates

---
+++ precache-manifest.9a4076de93665b2675c471b2552c4be5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "447f675b9eb0f44ec3fcd33976f772f2",
+    "url": "/todoList/index.html"
+  },
+  {
+    "revision": "30ca3ea3971d215b8606",
+    "url": "/todoList/static/css/main.43d428ff.chunk.css"
+  },
+  {
+    "revision": "dbf8206f7c87daedf843",
+    "url": "/todoList/static/js/2.d5279897.chunk.js"
+  },
+  {
+    "revision": "58aabe32aae325c8d2caf2ff6956fc48",
+    "url": "/todoList/static/js/2.d5279897.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30ca3ea3971d215b8606",
+    "url": "/todoList/static/js/main.1f60ee75.chunk.js"
+  },
+  {
+    "revision": "d301e1d585b3076bd681",
+    "url": "/todoList/static/js/runtime-main.a0ccc231.js"
+  }
+]);
